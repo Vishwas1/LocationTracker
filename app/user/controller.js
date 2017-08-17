e252a86feee0b683d96fdc40b16344180adc0e03
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2017-08-17 20:18:12 +0530

    Initial commit

--- app/user/controller.js
+++ app/user/controller.js
@@ -14,69 +14,77 @@ var userController = {
 			console.log('userController : usernameReq ='+ usernameReq);
 			passwordReq = body.password;
 			console.log('userController : passwordReq ='+ passwordReq);
-
-			if(global.dbConnection)
+			if(usernameReq != "" && passwordReq != "" && usernameReq != 'Undefined' && passwordReq != 'Undefined')
 			{
-				console.log('userController : Database connection success');
-				if(typeof global.dbQueryObj != 'Undefined')
+				if(global.dbConnection)
 				{
-					var query = { username :  usernameReq };
-					var OueryObj = global.dbQueryObj.collection("User1");
-					new Promise(function(resolve,reject){
-						console.log('userController : query = '+ query);
-						//Check if UserName exists 
-						OueryObj.find(query).toArray(function(err,result){
-							if(err) throw err;
-							console.log('userController : result = '+ result.length);
-							if(result.length == 0)
-								resolve(result);
-							else
-								reject('Alerady Exists');
-						});
-					})
-					.then(function(result){
-						//If not then insert - Resolve promise
-						console.log('userController : Promise1 resolved: User does not exists in Database ');
-						return new Promise(function(resolve,reject){
-							var userVar = new User({
-								username : usernameReq,
-								password : passwordReq
+					console.log('userController : Database connection success');
+					if(typeof global.dbQueryObj != 'Undefined')
+					{
+						var query = { username :  usernameReq };
+						var OueryObj = global.dbQueryObj.collection("User1");
+						new Promise(function(resolve,reject){
+							console.log('userController : query = '+ query);
+							//Check if UserName exists 
+							OueryObj.find(query).toArray(function(err,result){
+								if(err) throw err;
+								console.log('userController : result = '+ result.length);
+								if(result.length == 0)
+									resolve(result);
+								else
+									reject('Alerady Exists');
 							});
-							OueryObj.insertOne(userVar, function(err, result) {
-						    if (err) throw err;
-						    console.log('userController : Promise1 resolved: result.insertCount = '+ result.insertCount);
-						    if(result.insertedCount == 1)
-						    	resolve('Inserted');
-						    else
-						    	reject('Error');
+						})
+						.then(function(result){
+							//If not then insert - Resolve promise
+							console.log('userController : Promise1 resolved: User does not exists in Database ');
+							return new Promise(function(resolve,reject){
+								var userVar = new User({
+									username : usernameReq,
+									password : passwordReq
+								});
+								OueryObj.insertOne(userVar, function(err, result) {
+							    if (err) throw err;
+							    console.log('userController : Promise1 resolved: result.insertedCount = '+ result.insertedCount);
+							    if(result.insertedCount == 1)
+							    	resolve('Inserted');
+							    else
+							    	reject('Error');
+								});
 							});
+						}, function(err){
+							//Alerady exists in the db - Reject Promise - respond saying that user name already exists
+							console.log('userController : Promise1 rejected: User already exists in Database ');
+							return res.json({status: 'Error' , message : 'User already exisits Database'});
+						})
+						.then(function(result){
+							// Successfully inserted into db - 2nd Promise resolved
+							console.log('userController : Promise2 resolved: User created successfully ');
+							return res.status(200).json({message: 'user created successfully'});								
+						},function(err){
+							// Successfully inserted into db - 2nd Promise rejected
+							console.log('userController : Promise2 rejected: Could not insert into Database');
+							return res.json({status: 'Error' , message : 'Could not insert into Database'});
+						})
+						.catch(function(e){
+							console.log('userController :Error e.Message = '+ e.Message + ', e.Stack = '+ e.Stack);
 						});
-					}, function(err){
-						//Alerady exists in the db - Reject Promise - respond saying that user name already exists
-						console.log('userController : Promise1 rejected: User already exists in Database ');
-						return res.json({message: 'Error : User already exisits Database'});
-					})
-					.then(function(result){
-						// Successfully inserted into db - 2nd Promise resolved
-						console.log('userController : Promise2 resolved: User created successfully ');
-						return res.status(200).json({message: 'user created successfully'});								
-					},function(err){
-						// Successfully inserted into db - 2nd Promise rejected
-						console.log('userController : Promise2 rejected: Could not insert into Database');
-						return res.json({message: 'Error : Could not insert into Database'});
-					})
-					.catch(function(e){
-						console.log('userController :Error e.Message = '+ e.Message + ', e.Stack = '+ e.Stack);
-					});
+					}
+					else
+					{
+						console.log('userController : global.db is undefined');		
+					}
 				}
 				else
 				{
-					console.log('userController : global.db is undefined');		
+					console.log('userController : Database connection failed');
+					return res.json({status: 'Error' , message : 'Database connection failed'});
 				}
 			}
 			else
 			{
-				console.log('userController : Database connection failed');
+				console.log('userController : UserName or Password can not be null or empty');
+				return res.json({status: 'Error',  message : 'UserName or Password can not be null or empty'});
 			}
 		}
 		catch(err)
@@ -93,8 +101,18 @@ var userController = {
 			if(typeof global.dbQueryObj != 'Undefined')
 			{
 				var OueryObj = global.dbQueryObj.collection("User1");
-				OueryObj.find({}, {_id :false}).toArray(function(err, users) {
-				    res.json(users);
+				new Promise(function(resolve,reject){
+					OueryObj.find({}, {_id :false}).toArray(function(err, users) {
+						if(err) throw err;
+						else
+							resolve(users);
+					});
+				})
+				.then(function(users){
+					res.json(users);
+				})
+				.catch(function(e){
+					console.log('userController :Error e.Message = '+ e.Message + ', e.Stack = '+ e.Stack);
 				});
 			}
 			else
@@ -106,21 +124,8 @@ var userController = {
 		{
 			console.log('userController : Database connection failed');
 		}
-
 		console.log('userController : getusers method ends');
 	}
 }
-
-// async function InsertIntoDb(Model){
-// 	await Model.save();
-// 	// return new Promise(function(resolve,reject){
-// 	// 	Model.save(function(err) {
-// 	// 	   if(err) reject(err);
-// 	// 	   else
-// 	// 	   	resolve('Success!');
-// 	// 	});
-// 	// });
-// 	// Model.save();
-// }
 
 module.exports = userController;
--- server.js
+++ server.js
@@ -7,10 +7,7 @@ var express = require('express'),
  mongoose =require('mongoose'),
  mongoClient = require('mongodb').MongoClient,
  jwt = require('jsonwebtoken'),
- //await = require('asyncawait/await'),
- //async = require('asyncawait/async')
  dbConn = require('./dbConn');
-
 
 
 
@@ -28,21 +25,10 @@ dbConn.connection().then(function(res){
 });
 
 
-if(dbConn.connection){
-	console.log('server : Database is connected');
-}else{
-	console.log('server : Database is not connected');
-}
-
-
-
-
 //setting secret variable 
 console.log('server : Before setting secret key:' + config.secret);
 app.set('superSecret',config.secret);
 console.log('server : After setting secret key');
-
-
 
 //body-parser to get infos from POST URL parameters
 app.use(bodyparser.urlencoded({extended : false}));
@@ -50,8 +36,6 @@ app.use(bodyparser.json());
 
 //morgan to log requests to the console
 app.use(morgan('dev'));
-
-
 
 //Routes configuration ...
 /*app.get('/',function(req,res){
@@ -61,17 +45,7 @@ app.use(morgan('dev'));
 app.use('/user', require('./app/user'));
 
 
-
-
 //listen and start the server
 app.listen(port);
 console.log('Server is up and running at port:'+port);
-
-
-
-
-
-
-
-
 
