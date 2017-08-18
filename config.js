b8a70984c868b7cda64e7751b73e681f753593fa
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2017-08-18 20:51:33 +0530

    process env port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var env = require('./environment');
 
 var config = {
-	port : env.environment === 'development' ? 3000 :80,
+	port : env.environment === 'development' ? process.env.PORT || 3000 : process.env.PORT || 80,
 	secret : 'vishwasbanand',
 	database : 'mongodb://admin:mongo2017@vishwascluster-shard-00-00-g2gj6.mongodb.net:27017,vishwascluster-shard-00-01-g2gj6.mongodb.net:27017,vishwascluster-shard-00-02-g2gj6.mongodb.net:27017/test?ssl=true&replicaSet=VishwasCluster-shard-0&authSource=admin',
 	//database : 'mongodb://localhost:27017/charcoalAssignmentDb',
